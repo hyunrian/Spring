ed2de6185f197bff20c9fea4cbb4bf7ab1f43984
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-07-07 16:42:48 +0900

    @ModelAttribute, @Model, Lombok

--- 7월1주차/ex01/src/main/java/com/kh/ex01/controller/SampleController.java
+++ 7월1주차/ex01/src/main/java/com/kh/ex01/controller/SampleController.java
@@ -9,9 +9,20 @@ public class SampleController {
 
 	@RequestMapping(value = "/doA", method = RequestMethod.GET) 
 	// get방식으로 /doA라는 요청이 들어오면 실행
-	public String doA() {
+	public void doA() {
 		System.out.println("doA 실행됨");
-		return "doA"; // /WEB-INF/views/doA.jsp(model2할 때 nextPage forward한 것과 같음)
+//		return "doA"; 
+		// /WEB-INF/views/doA.jsp(model2할 때 nextPage forward한 것과 같음)
+		// servlet-context.xml에 미리 설정되어 있는 prefix, suffix가 있음
+		// 메서드 반환타입이 void인 경우 value로 설정한 값.jsp가 자동으로 실행됨
+	}
+	
+	// 설정한 값과 다르게 경로를 지정하고 싶은 경우 String으로 리턴하면 됨
+	@RequestMapping(value = "/doB", method = RequestMethod.GET)
+//	@RequestMapping(value = "/doB")
+	public String doB() {
+		System.out.println("doB 실행됨");
+		return "do_B";
 	}
 	
 }
