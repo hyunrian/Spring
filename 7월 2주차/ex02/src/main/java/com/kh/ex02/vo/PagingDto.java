1357580e45d1a69724b5a26a4c9632d8f7e20e79
HyunRian <121999215+hyunrian@users.noreply.github.com>
2023-07-12 16:32:57 +0900

    페이징 - n줄씩 보기 기능 구현

--- 7월 2주차/ex02/src/main/java/com/kh/ex02/vo/PagingDto.java
+++ 7월 2주차/ex02/src/main/java/com/kh/ex02/vo/PagingDto.java
@@ -1,11 +1,13 @@
 package com.kh.ex02.vo;
 
-import lombok.ToString;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@ToString
+@NoArgsConstructor
+@Data
 public class PagingDto {
 	
-	private int page; 			// 현재 페이지
+	private int page = 1; 		// 현재 페이지
 	private int startRow; 		// 시작 행번호(rownum between A and B에서 A에 해당)
 	private int endRow; 		// 끝 행번호(rownum between A and B에서 B에 해당)
 	private int startPage; 		// 페이지 리스트의 첫 페이지. 1 2 3 ... 9 10에서 1
@@ -15,30 +17,11 @@ public class PagingDto {
 	private int totalPage; 		// 전체 페이지 수
 	private final int PAGE_BLOCK_COUNT = 10; 
 	
-	public PagingDto(int page, int count) {
+	public PagingDto(int page, int perPage, int count) {
 		this.page = page;
+		this.perPage = perPage;
 		this.totalCount = count;
 		calc();
-	}
-	
-	public void setPerPage(int perPage) {
-		this.perPage = perPage;
-	}
-	
-	public int getStartPage() {
-		return startPage;
-	}
-	
-	public int getEndPage() {
-		return endPage;
-	}
-	
-	public int getTotalPage() {
-		return totalPage;
-	}
-	
-	public int getPage() {
-		return page;
 	}
 	
 	public void calc() {
